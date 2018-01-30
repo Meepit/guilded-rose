28769e6128261541ba7abb334ae1281551984da7
James Lizamore <jwlizamore@gmail.com>
2018-01-30 15:29:16 +0000

    Wrote first test, testing normal item degradation

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 import unittest
+from unittest import mock
+from unittest.mock import MagicMock
+import mock
 
 from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def setUp(self):
-        pass
-        #self.gilded_rose = GildedRose()
+        # Reassigning name is neccessary because name is an attribute of MagicMock..
+        self.normal_item = mock.Mock(name="foo", sell_in=0, quality=1)
+        self.normal_item.name = "foo"
 
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        self.gilded_rose = GildedRose(items)
-        self.gilded_rose.update_quality()
-        self.assertEquals("foo", items[0].name)
+    def test_normal_item_degradation(self):
+        gilded_rose = GildedRose([self.normal_item])
+        gilded_rose.update_quality()
+        self.assertEqual(0, self.normal_item.quality)
 
-    def test_aged_brie(self):
-        items = [Item("Aged Brie", 1, 0)]
-        self.gilded_rose = GildedRose(items)
-        self.gilded_rose.update_quality()
-        self.assertEquals(1, items[0].quality)
+
 
 if __name__ == '__main__':
     unittest.main()
