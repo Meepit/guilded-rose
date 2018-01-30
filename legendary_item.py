74fa5eddcf2951ee2581a56ef0cd6697ecc8e2e3
James Lizamore <jwlizamore@gmail.com>
2018-01-30 17:24:20 +0000

    Added 4 new item subclasses with basic skeletons

---
+++ legendary_item.py
@@ -0,0 +1,8 @@
+from Item import Item
+
+class LegendaryItem(Item):
+    def __init__(self, name, sell_in, quality):
+        Item.__init__(self, name, sell_in, quality)
+
+    def __repr__(self):
+        return Item.__repr__(self) + ", legendary item"
