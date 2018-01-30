6d8f044c3436b470bee474331d3ea86a92453f38
James Lizamore <jwlizamore@gmail.com>
2018-01-30 15:56:03 +0000

    Added skeleton structure for item tests

---
+++ test_item.py
@@ -0,0 +1,17 @@
+import unittest
+
+class ItemTest(unittest.TestCase):
+    def test_item_name(self):
+        pass
+
+    def test_item_sell_in(self):
+        pass
+
+    def test_item_quality(self):
+        pass
+
+    def test_item_print(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
